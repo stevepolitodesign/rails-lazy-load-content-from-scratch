f2d26547a6c1052dae16363dc9c5a21974b02385
Steve Polito <stevepolito@hey.com>
2021-09-30 06:18:27 -0400

    WIP: JSON Endpoints.

--- app/controllers/lazy_load/posts_controller.rb
+++ app/controllers/lazy_load/posts_controller.rb
@@ -1,2 +1,11 @@
 class LazyLoad::PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    respond_to do |format|
+      format.json { render json: @posts.to_json }
+    end
+  end
+
+  def show
+  end
 end
