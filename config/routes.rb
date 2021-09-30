36c79ba7a91411be5edbb2d0b5a484acdbd60054
Steve Polito <stevepolito@hey.com>
2021-09-30 06:18:17 -0400

    Fix syntax in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   resources :users
   resources :posts
+
+  namespace :lazy_load do
+    defaults format: :json do
+      resources :posts, only: [:index, :show]
+      resources :users, only: [:index,:show]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
