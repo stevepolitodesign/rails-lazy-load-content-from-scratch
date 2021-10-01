0204e4507611bcd4ddebaa92f27e65caa1dab48e
Steve Polito <stevepolito@hey.com>
2021-10-01 06:46:19 -0400

    Add target.

--- app/javascript/controllers/lazy_load_controller.js
+++ app/javascript/controllers/lazy_load_controller.js
@@ -1,6 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static values = { url: String }
+
   connect() {
     this.element.textContent = "Hello World!"
   }
