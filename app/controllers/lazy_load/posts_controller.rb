4ee24918cdad94f388f2bc55c12758498b9ed491
Steve Polito <stevepolito@hey.com>
2021-10-01 06:00:10 -0400

    Render individual post.

--- app/controllers/lazy_load/posts_controller.rb
+++ app/controllers/lazy_load/posts_controller.rb
@@ -1,4 +1,6 @@
 class LazyLoad::PostsController < ApplicationController
+  before_action :set_post, only: :show
+
   def index
     @posts = Post.all
     respond_to do |format|
@@ -7,5 +9,14 @@ class LazyLoad::PostsController < ApplicationController
   end
 
   def show
+    respond_to do |format|
+      format.json { render json: { html: render_to_string(partial: "lazy_load/posts/post", locals: { post: @post }, formats: [:html]) } }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
