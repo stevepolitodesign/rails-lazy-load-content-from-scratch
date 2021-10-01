1c48c1e877e6ef531360a91e0b539f59f368bb72
Steve Polito <stevepolito@hey.com>
2021-10-01 07:16:01 -0400

    WIP: Load content.

--- app/javascript/controllers/lazy_load_controller.js
+++ app/javascript/controllers/lazy_load_controller.js
@@ -1,9 +1,23 @@
+import Rails from "@rails/ujs"
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = [ "output" ]
   static values = { url: String }
 
   connect() {
-    this.element.textContent = "Hello World!"
+    this.fetchContent();
   }
+
+  fetchContent() {
+    const outputTarget = this.outputTarget;
+    Rails.ajax({
+      type: "GET",
+      url: this.urlValue,
+      success(data) {
+        outputTarget.innerHTML = data.html;
+      }
+    })
+  }
+
 }
