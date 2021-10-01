d5400630d67fb5a62f50919d21317ac7468e88e7
Steve Polito <stevepolito@hey.com>
2021-10-01 05:56:26 -0400

    WIP: Respond with partial

--- app/controllers/lazy_load/posts_controller.rb
+++ app/controllers/lazy_load/posts_controller.rb
@@ -2,7 +2,7 @@ class LazyLoad::PostsController < ApplicationController
   def index
     @posts = Post.all
     respond_to do |format|
-      format.json { render json: @posts.to_json }
+      format.json { render json: { html: render_to_string(partial: "lazy_load/posts/post", collection: @posts, formats: [:html]) } }
     end
   end
 
