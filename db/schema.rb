b6035d658bf0ca7c974be6bffe99766ff98ffd66
Steve Polito <stevepolito@hey.com>
2021-10-09 05:58:25 -0400

    Relate a Post to a User.
    * I figured this would make the tutorial more interesting by allowing me to add hover cards.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_100416) do
+ActiveRecord::Schema.define(version: 2021_10_09_095453) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -27,4 +29,5 @@ ActiveRecord::Schema.define(version: 2021_09_29_100416) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "posts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-100.times do |i|
-  Post.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph(sentence_count: 10))
+10.times do |i|
+  User.create!(name: Faker::Name.name, email: Faker::Internet.email, biography: Faker::Lorem.paragraph(sentence_count: 2))
 end
 
-10.times do |i|
-  User.create(name: Faker::Name.name, email: Faker::Internet.email, biography: Faker::Lorem.paragraph(sentence_count: 2))
-end
+User.all.each do |user|
+  user.posts.create!(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph(sentence_count: 10))
+end 
